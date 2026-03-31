68fff86d4d7e3b83ff7cb29a1c7a2e0fd0d781d5
ywyxyx@qq.com <ywyxyx@qq.com>
2026-03-31 17:43:13 +0800

    order list

--- ewshop/src/views/order/Order.vue
+++ ewshop/src/views/order/Order.vue
@@ -1,11 +1,42 @@
 <script setup>
+import Navbar from 'components/common/navbar/navbar.vue';
+import { useRoute, useRouter } from 'vue-router';
+import { useStore } from 'vuex';
+import { onMounted, computed, toRefs, reactive, ref } from 'vue';
+import { addAddress, EditAddress, DeleteAddress, getAddressDetail } from 'network/address';
+import { showToast, showSuccessToast, showFailToast, showLoadingToast, closeToast } from 'vant';
+const router = useRouter()
+const route = useRoute()
+const store = useStore()
 
+let tabs = ref(null);
+const state = reactive({
+    loading: false,
+    finished: false,
+    refreshing: false,
+    list: [],
+    page: 1,
+    totalPage: 0,
+    status: 0,
+})
+const {loading,finished,refreshing,list,page,totalPage,status} = toRefs(state)
 </script>
 
 <template>
+    <Navbar class="nav-bar">
+        <template v-slot:default>
+            定单列表
+        </template>
+    </Navbar>
 
+    <van-tabs ref="tabs" @click="onChangeTab" class="order-tab">
+        <van-tab title="全部"></van-tab>
+        <van-tab title="待付款"></van-tab>
+        <van-tab title="已支付"></van-tab>
+        <van-tab title="发货"></van-tab>
+        <van-tab title="交易完成"></van-tab>
+        <van-tab title="过期"></van-tab>
+    </van-tabs>
 </template>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
