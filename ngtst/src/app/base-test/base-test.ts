24706e166948ee3ab6848439b206a73c20fb80d1
ywyxyx@qq.com <ywyxyx@qq.com>
2026-04-13 20:06:02 +0800

    xiaochengxu

--- ngtst/src/app/base-test/base-test.ts
+++ ngtst/src/app/base-test/base-test.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
-
+import { NgOptimizedImage } from '@angular/common';
 @Component({
   selector: 'app-base-test',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './base-test.html',
   styleUrl: './base-test.scss',
 })
-export class BaseTest {}
+export class BaseTest {
+  aa = false;
+  logoUrl = '../assets/swkl.jpg';
+  logoAlt = 'Angular logo';
+  constructor() {
+    // setTimeout(() => {
+    //   this.viewport = true;
+    // }, 5000);
+   }
+
+
+}
