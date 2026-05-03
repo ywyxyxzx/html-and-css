848b9b092f582dccd9238e811d73438872d82d15
ywyxyx@qq.com <ywyxyx@qq.com>
2026-05-03 11:38:31 +0800

    redux中间件

--- react-test/3_quanjia/src/store.js
+++ react-test/3_quanjia/src/store.js
@@ -1,4 +1,13 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware} from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+/**
+ * Redux reducer for counter state management
+ * @param {number} state - Current state, defaults to 0
+ * @param {Object} action - Action object with type property
+ * @returns {number} New state
+ */
 function counter(state = 0, action) {
   switch (action.type) {
     case 'INCREMENT':
@@ -9,10 +18,10 @@ function counter(state = 0, action) {
       return state;
   }
 }
-const store = createStore(counter);
+const store = createStore(counter, applyMiddleware(logger, thunk));
 console.log(store.getState());
-store.dispatch({ type: 'INCREMENT' });
-console.log(store.getState());
-store.dispatch({ type: 'DECREMENT' });
-console.log(store.getState());
+// store.dispatch({ type: 'INCREMENT' });
+// console.log(store.getState());
+// store.dispatch({ type: 'DECREMENT' });
+// console.log(store.getState());
 export default store;
